5d97ea2b473132926fd2dca9e4439b97034b5d06
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-07 22:25:46 +0530

    Welcome Message

---
+++ StackAndQueues/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========================\nWelcome to Stack and Queue\n===========================");
+        }
+    }
+}
