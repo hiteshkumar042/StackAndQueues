dfd00c73bdf83a769b97c93bc4c094336b9f2947
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-07 22:32:45 +0530

    Created Stack

---
+++ StackAndQueues/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueues
+{
+    internal class Node
+    {
+        //Instances
+        public int data;
+        public Node next;
+        //Constructor
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
--- StackAndQueues/Program.cs
+++ StackAndQueues/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("===========================\nWelcome to Stack and Queue\n===========================");
+            //Calling Class and Adding Data in Stack
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+
+            //Display Message
+            linkedListStack.Display();
         }
     }
 }
