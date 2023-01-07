e6e37149a1e7b7133bfd1fed791486c28e4ef3fb
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-07 22:42:41 +0530

    Queue Added

--- StackAndQueues/Program.cs
+++ StackAndQueues/Program.cs
@@ -4,21 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("===========================\nWelcome to Stack and Queue\n===========================");
-            //Calling Class and Adding Data in Stack
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(70);
-            linkedListStack.Push(30);
-            linkedListStack.Push(56);
-            Console.WriteLine("----------------------------");
+            Console.WriteLine("===========================\nWelcome to Stack and Queue!\n===========================");
+            Console.WriteLine("1. Stack.\n2. Queue.");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1: //Stack
+                    //Calling Class and Adding Data in Stack
+                    LinkedListStack linkedListStack = new LinkedListStack();
+                    linkedListStack.Push(70);
+                    linkedListStack.Push(30);
+                    linkedListStack.Push(56);
+                    Console.WriteLine("----------------------------");
 
-            //Peek Value from Stack
-            linkedListStack.Peek();
-            Console.WriteLine("----------------------------");
-            linkedListStack.Pop();
+                    //Peek Value from Stack
+                    linkedListStack.Peek();
+                    Console.WriteLine("----------------------------");
+                    linkedListStack.Pop();
 
-            //Pop value till Stack will empty
-            linkedListStack.IsEmpty();
+                    //Pop value till Stack will empty
+                    linkedListStack.IsEmpty();
+                    break;
+                case 2: //Queue
+                    LinkedListQueue linkedListQueue = new LinkedListQueue();
+                    linkedListQueue.Enqueue(56);
+                    linkedListQueue.Enqueue(30);
+                    linkedListQueue.Enqueue(70);
+                    Console.WriteLine("----------------");
+
+                    //Display Queue
+                    linkedListQueue.Display();
+                    break;
+            }
         }
     }
 }
